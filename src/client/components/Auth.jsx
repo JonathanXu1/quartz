9b58a29a0b86726756fdc15b04f71f563c0c1134
Han Xiao <yhxiao97@gmail.com>
2019-09-25 20:12:50 -0400

    Adding Authentication Context Provider

--- src/client/components/Auth.jsx
+++ src/client/components/Auth.jsx
@@ -11,7 +11,7 @@ class Auth extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        user: null,
+        miner: null,
         authenticated: false,
         logout: this.logout,
         login: this.login,
@@ -21,7 +21,48 @@ class Auth extends Component {
 
   }
 
-  login(username, password) {
-    
+  login(email, password) {
+    axios.post(`${this.url}/v1/account/login`, 
+      {
+        email, 
+        password
+      })
+      .then(({data}) => {
+        this.updateAuth(data.accessToken);
+      })
+  }
+
+  logout() {
+    localStorage.removeItem('access_token');
+    this.setState({
+        authenticated: false,
+        accessToken: '',
+        miner: null,
+    });
+  }
+
+  updateAuth(accessToken) {
+    localStorage.setItem('access_token', accessToken);
+    const decodedToken = decode(accessToken);
+    this.setState({
+        miner: {
+            id: decodedToken.sub,
+            address: decodedToken.account.address,
+            name: decodedToken.account.name
+        },
+        jwtToken: accessToken,
+        authenticated: true,
+    });
+  }
+
+  render(){
+    return (
+      <authContext.Provider value={this.state}>
+          {this.props.children}
+      </authContext.Provider>
+  );
   }
 }
+
+export const AuthConsumer = authContext.Consumer;
+export const Auth = AuthComponent;
